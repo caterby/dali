c8d7ccf39f9511e3d5a51c466cf95925e08d6efa
caterby <caterby@163.com>
2021-10-19 01:18:38 -0700

    Add files via upload

---
+++ dali/operators/reader/loader/file_loader_new.cc
@@ -0,0 +1,272 @@
+// Copyright (c) 2017-2019, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <dirent.h>
+#include <errno.h>
+#include <memory>
+#include <chrono>
+#include "dali/core/common.h"
+#include "dali/operators/reader/loader/file_loader_new.h"
+#include "dali/util/file.h"
+#include "dali/operators/reader/loader/utils.h"
+
+#include <chrono>
+typedef std::chrono::high_resolution_clock Time;
+typedef std::chrono::milliseconds ms;
+typedef std::chrono::microseconds us;
+typedef std::chrono::duration<float> fsec;
+
+
+namespace dali {
+
+
+//template<int I, int J, class... T>
+template<class... T>
+inline auto to_pair(std::tuple<T...> t)
+    -> decltype(std::make_pair(std::get<0>(t), std::get<1>(t))) {
+  return std::make_pair(std::get<0>(t), std::get<1>(t));
+}
+
+inline void assemble_file_list(const std::string& path, const std::string& curr_entry, int label,
+                        std::vector<std::pair<std::string, int>> *file_label_pairs) {
+  std::string curr_dir_path = path + "/" + curr_entry;
+  DIR *dir = opendir(curr_dir_path.c_str());
+
+  struct dirent *entry;
+
+  while ((entry = readdir(dir))) {
+    std::string full_path = curr_dir_path + "/" + std::string{entry->d_name};
+#ifdef _DIRENT_HAVE_D_TYPE
+    /*
+     * we support only regular files and symlinks, if FS returns DT_UNKNOWN
+     * it doesn't mean anything and let us validate filename itself
+     */
+    if (entry->d_type != DT_REG && entry->d_type != DT_LNK &&
+        entry->d_type != DT_UNKNOWN) {
+      continue;
+    }
+#endif
+    std::string rel_path = curr_entry + "/" + std::string{entry->d_name};
+    if (HasKnownExtension(std::string(entry->d_name))) {
+      //file_label_pairs->push_back(std::make_tuple(full_path, label));
+      file_label_pairs->push_back(std::make_pair(rel_path, label));
+    }
+  }
+  closedir(dir);
+}
+
+inline size_t getFilesize(const char* filename) {
+    struct stat st;
+    if(stat(filename, &st) != 0) {
+        return 0;
+    }
+    return st.st_size;   
+}
+
+
+inline bool is_cached(std::string name){
+   struct stat   buffer;
+   const char* name_c = ("/dev/shm/cache/" + name).c_str();
+   return (stat (name_c, &buffer) == 0);
+}
+
+vector<std::pair<std::string, int>> filesystem::traverse_directories(const std::string& file_root) {
+  // open the root
+  DIR *dir = opendir(file_root.c_str());
+
+  DALI_ENFORCE(dir != nullptr,
+      "Directory " + file_root + " could not be opened.");
+
+  struct dirent *entry;
+
+  std::vector<std::pair<std::string, int>> file_label_pairs;
+  std::vector<std::string> entry_name_list;
+
+  while ((entry = readdir(dir))) {
+    struct stat s;
+    std::string entry_name(entry->d_name);
+    std::string full_path = file_root + "/" + entry_name;
+    int ret = stat(full_path.c_str(), &s);
+    DALI_ENFORCE(ret == 0,
+        "Could not access " + full_path + " during directory traversal.");
+    if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) continue;
+    if (S_ISDIR(s.st_mode)) {
+      entry_name_list.push_back(entry_name);
+    }
+  }
+  // sort directories to preserve class alphabetic order, as readdir could
+  // return unordered dir list. Otherwise file reader for training and validation
+  // could return directories with the same names in completely different order
+  std::sort(entry_name_list.begin(), entry_name_list.end());
+  for (unsigned dir_count = 0; dir_count < entry_name_list.size(); ++dir_count) {
+      assemble_file_list(file_root, entry_name_list[dir_count], dir_count, &file_label_pairs);
+  }
+  // sort file names as well
+  std::sort(file_label_pairs.begin(), file_label_pairs.end());
+  printf("read %lu files from %lu directories\n", file_label_pairs.size(), entry_name_list.size());
+
+  closedir(dir);
+  //for (int i = 0; i < static_cast<int>(file_label_pairs.size()); i++)
+  //  std::cout << file_label_pairs[i].first << ", " << file_label_pairs[i].second << std::endl;
+
+  return file_label_pairs;
+}
+
+void FileLoader::PrepareEmpty(ImageLabelWrapper &image_label) {
+  PrepareEmptyTensor(image_label.image);
+}
+
+void FileLoader::ReadSample(ImageLabelWrapper &image_label) {
+  //std::tuple<std::string, int> image_tuple = image_label_pairs_[current_index_++];
+  auto image_pair = image_label_pairs_[current_index_++];
+  //auto image_pair = std::make_pair(std::get<0>(image_tuple), std::get<1>(image_tuple));
+  int cur_idx = current_index_ - 1;
+  //std::cout << "Reading Current index = " << cur_idx << ", img = " << image_pair.first << std::endl;
+
+  // handle wrap-around
+  MoveToNextShard(current_index_);
+  auto start_all = Time::now();
+  // copy the label
+  image_label.label = image_pair.second;
+  DALIMeta meta;
+  meta.SetSourceInfo(image_pair.first);
+  meta.SetSkipSample(false);
+
+  // if image is cached, skip loading
+  if (ShouldSkipImage(image_pair.first)) {
+    meta.SetSkipSample(true);
+    image_label.image.Reset();
+    image_label.image.SetMeta(meta);
+    image_label.image.set_type(TypeInfo::Create<uint8_t>());
+    image_label.image.Resize({0});
+    return;
+  }
+  
+  /*
+   * We need to do is update the path of the file to point to the memory area instead of disk
+   * If cache size is not full, then add this entry to cache,else do nothing
+  */
+  //std::cout << "SHM cache list length " << shm_cache_index_list_.size() << endl;
+  if (cache_size_ > 0 && !caching_done_){
+      bool must_cache = std::binary_search (shm_cache_index_list_.begin(), shm_cache_index_list_.end(), cur_idx);
+      //std::cout << "Searching for " << image_pair.first << " found : " << must_cache << " cache done? : " << caching_done_ << endl; 
+      if (!caching_done_ && must_cache) {
+        RawDataDache::CacheRawDataEntry *ce = new shm::CachRawDataeEntry(image_pair.first);
+        int ret = -1;
+        ret = ce->create_segment();
+        DALI_ENFORCE(ret != -1,
+          "Cache for " + image_pair.first + " could not be created.");
+
+        ret = ce->put_cache_simple(file_root_ + "/" + image_pair.first);
+        DALI_ENFORCE(ret != -1,
+          "Cache for " + image_pair.first + " could not be populated.");
+        shm_cached_items_.push_back(image_pair.first);
+ 
+
+        //ret = ce->close_segment();
+        //DALI_ENFORCE(ret != -1,
+        //  "Cache for " + image_pair.first + " could not be closed.");
+
+        delete ce;
+      }
+  }
+
+  // check if cached
+  // Change this to be parameter. Hardcoded for now
+  bool use_prefix = true;
+  std::string prefix;
+  prefix = file_root_;
+  int node = -1;
+  
+  if (cache_size_ > 0 && caching_done_){
+        // Check current node
+        //auto start = Time::now();
+	if (is_cached(image_pair.first)){
+    		prefix = "/dev/shm/cache";
+    		//std::cout << "Got local cached value for " << image_pair.first << endl;
+	}
+	//Check other nodes
+	else if ((node = shm::is_cached_in_other_node(shm_cache_index_list_other_nodes, image_pair.first, node_id_)) >= 0) {
+                //std::cout << "Available in node " << node << " for " << image_pair.first << " on " << server_fd_[node] << endl;
+		use_prefix = false;
+      		string fn = file_root_ + "/" + image_pair.first;
+      		long long image_size = getFilesize(fn.c_str());
+	    	//std::cout << "\tImage size " << image_size << " for " << image_pair.first << endl;
+		if (image_size < 0) {
+			//std::cout << "Unsuccessful header read from node " << node << " for " << image_pair.first << endl;
+			prefix = file_root_;
+			use_prefix = true;
+		}
+		else { 
+			if (image_label.image.shares_data()) {
+				image_label.image.Reset();
+			}
+			image_label.image.Resize({image_size});
+        		net_mutex_.lock();
+			int bytes_read = shm::ReadFromOtherNode(rank[node], image_pair.first, image_label.image.mutable_data<uint8_t>(), image_size);
+        
+        		net_mutex_.unlock();		
+			if (bytes_read < 0){
+				//std::cout << "Unsuccessful read from node " << node << " for " << image_pair.first << endl;
+				prefix = file_root_;
+				use_prefix = true;
+			}
+			else{
+    				//std::cout << "Got remote cached value for " << image_pair.first << endl;
+			}
+  		}
+	}
+        else
+		std::cout << "Sample " << image_pair.first << " not found in any cache. Must fetch" << endl;
+  }
+  else
+    prefix = file_root_;
+
+
+	if (use_prefix) {
+  		//auto current_image = FileStream::Open(image_pair.first, read_ahead_);
+  		//auto current_image = FileStream::Open(file_root_ + "/" + image_pair.first, read_ahead_);
+  		//outfile << "\tReading " << prefix << "/" << image_pair.first << endl;
+  		auto current_image = FileStream::Open(prefix + "/" + image_pair.first, read_ahead_);
+  		Index image_size = current_image->Size();
+
+  		if (copy_read_data_) {
+    			//std::cout << "Copying " << image_pair.first << std::endl;
+    			if (image_label.image.shares_data()) {
+      				image_label.image.Reset();
+    			}
+    		image_label.image.Resize({image_size});
+    		// copy the image
+    		current_image->Read(image_label.image.mutable_data<uint8_t>(), image_size);
+  		} else {
+    			//std::cout << "Sharing " << image_pair.first << std::endl;
+    			auto p = current_image->Get(image_size);
+    			// Wrap the raw data in the Tensor object.
+    			image_label.image.ShareData(p, image_size, {image_size});
+    			image_label.image.set_type(TypeInfo::Create<uint8_t>());
+  		}
+
+  		// close the file handle
+  		current_image->Close();
+	}
+
+  // copy the label
+  image_label.label = image_pair.second;
+  image_label.image.SetMeta(meta);
+}
+
+Index FileLoader::SizeImpl() {
+  return static_cast<Index>(image_label_pairs_.size());
+}
+}  // namespace dali
---
+++ dali/operators/reader/loader/file_loader_new.h
@@ -0,0 +1,332 @@
+// Copyright (c) 2017, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef DALI_OPERATORS_READER_LOADER_FILE_LOADER_NEW_H_
+#define DALI_OPERATORS_READER_LOADER_FILE_LOADER_NEW_H_
+
+#include <dirent.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+#include <fstream>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <thread>
+
+#include "dali/core/common.h"
+#include "dali/operators/reader/loader/loader.h"
+#include "dali/operators/shmcache/raw_data_cache.h"
+#include "dali/util/file.h"
+
+#include<sys/types.h> 
+#include<netinet/in.h>   
+#include<netdb.h> 
+#include<sys/uio.h>    
+#include<sys/syscall.h>  
+#include<unistd.h>  
+#include<fcntl.h>
+#include "commands.h"
+
+namespace dali {
+
+namespace filesystem {
+
+vector<std::pair<string, int>> traverse_directories(const std::string& path);
+
+}  // namespace filesystem
+
+struct ImageLabelWrapper {
+  Tensor<CPUBackend> image;
+  int label;
+};
+
+
+class FileLoader : public Loader<CPUBackend, ImageLabelWrapper> {
+ public:
+  explicit inline FileLoader(
+    const OpSpec& spec,
+    vector<std::pair<string, int>> image_label_pairs = std::vector<std::pair<string, int>>(),
+    bool shuffle_after_epoch = false)
+    : Loader<CPUBackend, ImageLabelWrapper>(spec),
+      file_root_(spec.GetArgument<string>("file_root")),
+      file_list_(spec.GetArgument<string>("file_list")),
+      image_label_pairs_(std::move(image_label_pairs)),
+      shuffle_after_epoch_(shuffle_after_epoch),
+      current_index_(0),
+      current_epoch_(0),
+      dist_mint(false),	
+      caching_done_(false) 
+        
+
+      /*
+      * Those options are mutually exclusive as `shuffle_after_epoch` will make every shard looks differently
+      * after each epoch so coexistence with `stick_to_shard` doesn't make any sense
+      * Still when `shuffle_after_epoch` we will set `stick_to_shard` internally in the FileLoader so all
+      * DALI instances will do shuffling after each epoch
+      */
+      if (shuffle_after_epoch_ || stick_to_shard_)
+        DALI_ENFORCE(
+          !shuffle_after_epoch_ || !stick_to_shard_,
+          "shuffle_after_epoch and stick_to_shard cannot be both true");
+      if (shuffle_after_epoch_ || shuffle_)
+        DALI_ENFORCE(
+          !shuffle_after_epoch_ || !shuffle_,
+          "shuffle_after_epoch and random_shuffle cannot be both true");
+      /*
+       * Imply `stick_to_shard` from  `shuffle_after_epoch`
+       */
+      if (shuffle_after_epoch_) {
+        stick_to_shard_ = true;
+      }
+    mmap_reserver = FileStream::FileStreamMappinReserver(
+        static_cast<unsigned int>(initial_buffer_fill_));
+    copy_read_data_ = !mmap_reserver.CanShareMappedData();
+  }
+
+  void PrepareEmpty(ImageLabelWrapper &tensor) override;
+  void ReadSample(ImageLabelWrapper &tensor) override;
+  
+  ~FileLoader() {
+      //if (prefetcher_running){
+      //    mint_prefetcher.~thread();
+      //    mint_prefetcher.join();
+      //}
+     //std::cout << "Order of shm cached items : " << endl;
+     //for(int i=0; i < static_cast<int>(shm_cached_items_.size()); i++)
+         //std::cout << i << " : " << shm_cached_items_[i] << endl;
+   }
+
+ protected:
+  Index SizeImpl() override;
+
+  void PrepareMetadataImpl() override {
+    std::cout << "Num Shards:" << num_shards_ << "\nShardID:" << shard_id_ << "\nShuffle seed:"<< kDaliDataloaderSeed + shuffle_seed_ << std::endl;
+    if (image_label_pairs_.empty()) {
+      if (file_list_ == "") {
+        image_label_pairs_ = filesystem::traverse_directories(file_root_);
+      } else {
+        // load (path, label) pairs from list
+        std::ifstream s(file_list_);
+        DALI_ENFORCE(s.is_open(), "Cannot open: " + file_list_);
+
+        string image_file;
+        int label;
+        while (s >> image_file >> label) {
+          auto p = std::make_pair(image_file, label);
+          image_label_pairs_.push_back(p);
+        }
+        DALI_ENFORCE(s.eof(), "Wrong format of file_list: " + file_list_);
+      }
+    }
+    DALI_ENFORCE(Size() > 0, "No files found.");
+
+    Index size_per_shard = Size()/num_shards_;
+    if (cache_size_orig_ > size_per_shard) {
+      extra_cache_size_ = cache_size_orig_ - size_per_shard;
+      cache_size_ = size_per_shard;
+    }
+    else
+      cache_size_ = cache_size_orig_;
+
+    //std::cout << "Per shard = " << size_per_shard << ", cache = " << cache_size_ << ", extra cache = " << extra_cache_size_ << ", orig cache = "<<cache_size_orig_ << endl;
+
+    if (shuffle_) {
+      // seeded with hardcoded value to get
+      // the same sequence on every shard
+      std::mt19937 g(kDaliDataloaderSeed + shuffle_seed_);
+      std::shuffle(image_label_pairs_.begin(), image_label_pairs_.end(), g);
+    }
+
+    //if (current_epoch_ == 0)
+    //  image_label_pairs_orig_ = image_label_pairs_;
+
+    Reset(true);
+    //for (int i = 0; i < static_cast<int>(image_label_pairs_.size()); i++)
+    //    outfile << i << " : " << std::get<0>(image_label_pairs_[i]) << ", " << std::get<1>(image_label_pairs_[i]) << std::endl;
+  }
+
+  /* This function is called before the use of FileLoader iter
+   * in each epoch. SO any state that needs to be set at 
+   * epoch boundaries can be done here
+   */
+  void Reset(bool wrap_to_shard) override {
+    if (wrap_to_shard) {
+      std::cout << "Num Shards:" << num_shards_ << "\nShardID:" << shard_id_ << "\nShuffle seed:"<< kDaliDataloaderSeed + shuffle_seed_ << std::endl;
+      current_index_ = start_index(shard_id_, num_shards_, Size());
+      index_start_ = current_index_;
+      index_end_ = current_index_ + Size()/num_shards_;
+    } else {
+      current_index_ = 0;
+      index_start_ = current_index_;
+      index_end_ = current_index_ + Size()/num_shards_;
+    }
+      //std::cout << "Current Epoch : " << current_epoch_ << endl;
+      //std::cout << "Cache size : " << cache_size_  << "\nCached : " << caching_done_ << endl;
+      //std::cout << "Current index : " << current_index_ << "\nindex_start : " << index_start_ << "\nindex_end : " << index_end_ << endl;
+
+    if (shuffle_after_epoch_) {
+      std::mt19937 g(kDaliDataloaderSeed + shuffle_seed_ + current_epoch_);
+      std::shuffle(image_label_pairs_.begin(), image_label_pairs_.end(), g);
+    }
+    // If the epoch count is 1 here, it means we have completed epoch 1 
+    if (current_epoch_ == 1){
+       caching_done_ = true;
+       if ( num_nodes_ > 1 && cache_size_ > 0 && !resume_) { 
+           // if we have extra items to cache, handle here
+           if (extra_cache_size_ > 0 && dist_mint){
+               //std::cout << "EXTRA data cache for " << shard_id_ << endl;
+               vector<string> items_not_in_node;
+               for (unsigned int it = 0; it < num_nodes_; it ++){
+                  if (it != node_id_)
+                     items_not_in_node.insert(items_not_in_node.end(), shm_cache_index_list_other_nodes[it].begin(), shm_cache_index_list_other_nodes[it].end());
+               }
+               // Get a random shuffle order at each node so that what is in cache of each node is random
+               // halps balance nodes to remote caches
+               std::mt19937 gen_s(shuffle_seed_ + node_id_);
+               std::shuffle(items_not_in_node.begin(), items_not_in_node.end(), gen_s);
+               //std::cout << "Items not cached in current node = " << items_not_in_node.size() << endl;
+               Index items_per_shard = items_not_in_node.size()/num_shards_per_node_;
+               //std::cout << "Extra items required per shard = " << extra_cache_size_ << ", available indexes per shard " << items_per_shard << endl;
+               Index start_idx = (shard_id_ % num_shards_per_node_)*extra_cache_size_;
+               Index end_idx = (shard_id_ % num_shards_per_node_ + 1)*extra_cache_size_;
+               //std::cout << "Shard " << shard_id_ << " start : " << start_idx << ", end : " << end_idx << endl;
+               mint_prefetcher = std::thread(shm::PrefetchRawData, items_not_in_node, start_idx, end_idx, file_root_);
+               prefetcher_running = true;
+           }
+
+        }
+    }
+
+    // Create a shuffled list for caching   
+    // Sort it so that search becomes easier
+    if (!caching_done_ && cache_size_ > 0){
+       //std::cout << "Seed is " << shuffle_seed_ << endl;
+
+       //Get the cache list for other nodes
+       if ( num_nodes_ > 1) {
+           shm_cache_index_list_other_nodes.resize(num_nodes_);
+           for (int nid = 0; nid < num_nodes_; nid ++ ){
+               if (nid == node_id_){
+                   // We are in the current node; do nothing
+                   continue;
+               }
+               vector<string> nid_list = shm_cache_index_list_other_nodes[nid];
+               //std::cout << "For node " << nid << std::endl;
+               // Resize list to the total size of shards in this node
+               //nid_list.resize(Size()/num_nodes_);
+               for (int sh = 0; sh < num_shards_per_node_; sh ++){
+                   std::mt19937 gen(shuffle_seed_);
+                   Index shard_start_idx = start_index(num_shards_per_node_*nid + sh, num_shards_, Size());
+                   Index shard_end_idx = shard_start_idx + Size()/num_shards_;
+                   Index shard_size = shard_end_idx -  shard_start_idx;
+                   vector<int> cache_list_per_shard(shard_size);
+                   //std::cout << "\tShard " << nid*num_shards_per_node_ + sh << ", size " << shard_size << ", nid " << nid <<  std::endl;
+                   //std::cout << "\t\t Index begin " << shard_start_idx << ", index end " << shard_end_idx << std::endl;
+                   std::iota(cache_list_per_shard.begin(), cache_list_per_shard.end(), shard_start_idx);
+                   std::shuffle(cache_list_per_shard.begin(), cache_list_per_shard.end(), gen);
+                   cache_list_per_shard.resize(cache_size_);
+                   std::sort (cache_list_per_shard.begin(), cache_list_per_shard.end());
+                   vector<string> cache_list_per_shard_name;
+                   //std::cout << "For node " << nid <<  " shard : " << sh << endl;
+                   for (int k=0; k < cache_list_per_shard.size(); k++) {
+                     cache_list_per_shard_name.push_back(image_label_pairs_[cache_list_per_shard[k]].first);
+		     //std::cout << "\t" << k << " : " << cache_list_per_shard_name[k] << std::endl; 
+                   }
+                   nid_list.insert(nid_list.end(), cache_list_per_shard_name.begin(), cache_list_per_shard_name.end());
+               }
+               std::sort (nid_list.begin(), nid_list.end()); 
+               shm_cache_index_list_other_nodes[nid] = nid_list;
+           }
+
+
+           
+
+       }
+       /*for (int nid = 0; nid < num_nodes_; nid ++){
+           if (shm_cache_index_list_other_nodes[nid].size() > 0){
+	   std::cout << "For node " << nid << endl;
+               for (int i = 0; i < static_cast<int>(shm_cache_index_list_other_nodes[nid].size()); i++) 
+	       std::cout << "\t" << i << " : " << shm_cache_index_list_other_nodes[nid][i] << std::endl;
+           }
+       }*/
+
+       //shm_cache_index_list_.resize(cache_size_);
+       std::mt19937 gen(shuffle_seed_);
+       //std::uniform_int_distribution<int> distr(index_start_, index_end_); 
+       //std::generate(shm_cache_index_list_.begin(), shm_cache_index_list_.end(), [&](){ return distr(gen); });
+       shm_cache_index_list_.resize(Size()/num_shards_);
+       std::iota(shm_cache_index_list_.begin(), shm_cache_index_list_.end(), index_start_);
+       std::shuffle(shm_cache_index_list_.begin(), shm_cache_index_list_.end(),gen);
+       shm_cache_index_list_.resize(cache_size_);
+       std::sort (shm_cache_index_list_.begin(), shm_cache_index_list_.end());
+       vector<string> shm_cache_name_list_;
+       for (int k=0; k < shm_cache_index_list_.size(); k++)
+           shm_cache_name_list_.push_back(image_label_pairs_[shm_cache_index_list_[k]].first);
+       
+
+       std::cout << "Index list to cache for this shard : " << endl;
+       for (int i = 0; i < static_cast<int>(shm_cache_index_list_.size()); i++)
+          //std::cout << i << " : " << shm_cache_index_list_[i]  << " : " << shm_cache_name_list_[i] << std::endl;
+    }
+       
+
+    current_epoch_++;
+
+    if (resume_)
+        caching_done_ = true;
+
+  }
+
+  using Loader<CPUBackend, ImageLabelWrapper>::shard_id_;
+  using Loader<CPUBackend, ImageLabelWrapper>::shuffle_seed_;
+  using Loader<CPUBackend, ImageLabelWrapper>::cache_size_orig_;
+  using Loader<CPUBackend, ImageLabelWrapper>::num_shards_;
+  using Loader<CPUBackend, ImageLabelWrapper>::num_nodes_;
+  using Loader<CPUBackend, ImageLabelWrapper>::node_id_;
+  using Loader<CPUBackend, ImageLabelWrapper>::resume_;
+  using Loader<CPUBackend, ImageLabelWrapper>::seed_;
+  using Loader<CPUBackend, ImageLabelWrapper>::outfile;
+  using Loader<CPUBackend, ImageLabelWrapper>::num_shards_per_node_;
+
+  string file_root_, file_list_;
+  int extra_cache_size_ = 0;
+  int cache_size_ = 0;
+  //A map of file paths, label and a bool that indicates whether cached
+  vector<std::pair<string, int>> image_label_pairs_;
+//  vector<std::pair<string, int>> image_label_pairs_orig_;
+  bool shuffle_after_epoch_;
+  Index current_index_;
+  int current_epoch_;
+  bool caching_done_;
+  int index_start_;
+  int index_end_;
+  bool dist_mint = false;
+  std::thread mint_prefetcher;
+  bool prefetcher_running = false;
+  //int num_shards_per_node_ = num_shards_ / num_nodes_;
+  //vector<int> current_shards_;
+  //vector<int> current_shards_(num_shards_per_node_);
+  //std::iota(current_shards_.begin(), current_shards_.end(), node_id_*num_shards_per_node_);
+  vector<int> shm_cache_index_list_;
+  vector<vector<string>> shm_cache_index_list_other_nodes;
+  vector<std::string> shm_cached_items_;
+  FileStream::FileStreamMappinReserver mmap_reserver;
+};
+
+}  // namespace dali
+
+#endif  // DALI_OPERATORS_READER_LOADER_FILE_LOADER_NEW_H_
